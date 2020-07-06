b985377e7332852ec3108ef79231e1dcc7feda7e
Alan Nascimento <alan.silva.n@hotmail.com>
2020-07-06 10:54:40 -0300

    test: ensure SignUpController returns 400 if no name is provided

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
