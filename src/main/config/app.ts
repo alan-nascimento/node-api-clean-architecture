3f761e1ad27e8cf7ebbb3faa93ba7cc3c93c7e03
Alan Nascimento <alan.silva.n@hotmail.com>
2020-07-10 01:33:42 -0300

    refactor: move middlewares to barel

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,6 @@
 import express from 'express'
 
-import { bodyParser } from '../middlewares/body-parser'
-import { cors } from '../middlewares/cors'
-import { contentType } from '../middlewares/content-type'
+import { cors, bodyParser, contentType } from '../middlewares'
 
 class App {
   public server: express.Application = express()
