acbd1a47f3ebb931f8d0b2df6577b11aab607b10
Alan Nascimento <alan.silva.n@hotmail.com>
2020-07-17 14:15:16 -0300

    test: ensure ValidationComposite not returns if succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -48,4 +48,12 @@ describe('Validation Composite', () => {
 
     expect(error).toEqual(new Error())
   })
+
+  it('should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toBeFalsy()
+  })
 })
