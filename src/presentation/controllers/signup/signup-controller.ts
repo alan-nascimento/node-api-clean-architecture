1f378a41f9e78857e308accf53cfc61fb6737a4b
Alan Nascimento <alan.silva.n@hotmail.com>
2020-07-21 02:36:00 -0300

    refactor: improve signup controller constructor

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -2,13 +2,10 @@ import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
 import { HttpRequest, HttpResponse , Controller, AddAccount, Validation } from './signup-protocols-controller'
 
 export class SignUpController implements Controller {
-  private readonly addAccount: AddAccount
-  private readonly validation: Validation
-
-  constructor (addAccount: AddAccount, validation: Validation) {
-    this.addAccount = addAccount
-    this.validation = validation
-  }
+  constructor (
+    private readonly addAccount: AddAccount,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
