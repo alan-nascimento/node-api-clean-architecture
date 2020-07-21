a429d325d4796609b7b169bf00b1ee48e2d90bab
Alan Nascimento <alan.silva.n@hotmail.com>
2020-07-21 03:17:54 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -53,5 +53,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        })
+        .expect(401)
+    })
   })
 })
