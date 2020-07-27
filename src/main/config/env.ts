5aed4fd46bfe764456afc910a8f8c70b08c04640
Alan Nascimento <alan.silva.n@hotmail.com>
2020-07-27 00:13:06 -0300

    chore: change dev script to use docker-comppose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,5 @@
-import 'dotenv/config'
-
 export default {
-  mongoUrl: process.env.MONGO_URL,
-  port: process.env.PORT,
-  jwtSecret: process.env.JWT_SECRET
+  port: process.env.PORT || 5000,
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/node-api-clean-architecture',
+  jwtSecret: process.env.JWT_SECRET || '4fe889f73fb6b04f5cd5f853c5cfd5bd'
 }
