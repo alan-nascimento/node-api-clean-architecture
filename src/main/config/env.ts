eefa06d6139ac02fa114dae00d3f6f55bd5bfd2d
Alan Nascimento <alan.silva.n@hotmail.com>
2020-07-26 22:58:41 -0300

    chore: setup dotenv configs

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config'
+
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5000,
-  jwtSecret: process.env.JWT_SECRET || '4fe889f73fb6b04f5cd5f853c5cfd5bd'
+  mongoUrl: process.env.MONGO_URL,
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET
 }
