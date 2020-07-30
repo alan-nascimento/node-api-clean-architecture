38b542b358f8264f609fadb5e552666b4519895a
Alan Nascimento <alan.silva.n@hotmail.com>
2020-07-30 17:43:11 -0300

    feat: ensure DbLoadAccountByToken calls decrypter with correct values

---
+++ src/data/usecases/load-account-by-token/load-account-by-token.spec.ts
@@ -0,0 +1,40 @@
+import { Decrypter } from '../../protocols/criptography/decrypter'
+import { DbLoadAccountByToken } from './load-account-by-token'
+
+const makeDecrypter = (): any => {
+  class DecrypterStub implements Decrypter {
+    async decrypt (value: string): Promise<string> {
+      return Promise.resolve('any_value')
+    }
+  }
+
+  return new DecrypterStub()
+}
+
+interface SutTypes {
+  sut: DbLoadAccountByToken
+  decrypterStub: Decrypter
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = makeDecrypter()
+
+  const sut = new DbLoadAccountByToken(decrypterStub)
+
+  return {
+    sut,
+    decrypterStub
+  }
+}
+
+describe('DbLoadAccountByToken Usecase', () => {
+  it('should call decrypter with correct values', async () => {
+    const { sut, decrypterStub } = makeSut()
+
+    const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
+
+    await sut.load('any_token')
+
+    expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+})
---
+++ src/data/usecases/load-account-by-token/load-account-by-token.ts
@@ -0,0 +1,15 @@
+import { AccountModel } from '../../../domain/models/account'
+import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
+import { Decrypter } from '../../protocols/criptography/decrypter'
+
+export class DbLoadAccountByToken implements LoadAccountByToken {
+  constructor (
+    private readonly decrypter: Decrypter
+  ) {}
+
+  async load (accessToken: string, role?: string): Promise<AccountModel> {
+    await this.decrypter.decrypt(accessToken)
+
+    return null
+  }
+}
